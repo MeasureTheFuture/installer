65773cac06c4723e570202c3df77055948b37ac2
clinton <clinton.freeman@gmail.com>
2016-09-16 21:01:14 +1000

    fixes #11

--- mtf-install.sh
+++ mtf-install.sh
@@ -118,7 +118,7 @@ echo -ne "Configuring postgreSQL... Done\n"
 
 # Install MTF.
 echo -ne "Installing MTF..."
-opkg install http://reprage.com/ipkgs/mtf_0.0.9_x86.ipk &> /dev/null
+opkg install http://reprage.com/ipkgs/mtf_0.0.10_x86.ipk &> /dev/null
 echo -ne " Done\n"
 
 # Migrate database to the latest version.
