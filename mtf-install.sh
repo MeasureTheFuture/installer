34f390c29b1ccdfb4364fe0e74dc971d11a8a86d
clinton <clinton.freeman@gmail.com>
2016-09-24 13:07:42 +1000

    Released 0.0.13.

--- mtf-install.sh
+++ mtf-install.sh
@@ -120,7 +120,7 @@ echo -ne "Configuring postgreSQL... Done\n"
 
 # Install MTF.
 echo -ne "Installing MTF..."
-opkg install http://reprage.com/ipkgs/mtf_0.0.12_x86.ipk &> /dev/null
+opkg install http://reprage.com/ipkgs/mtf_0.0.13_x86.ipk &> /dev/null
 echo -ne " Done\n"
 
 # Migrate database to the latest version.
